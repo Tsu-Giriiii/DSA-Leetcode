bee44f5d8a7a77685aa85f3d7ca66ff4ff299a17
Giridhar Bargaley <153367525+Tsu-Giriiii@users.noreply.github.com>
2026-07-07 21:31:47 +0530

    Time: 0 ms (100%), Space: 23.1 MB (61.67%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        
+        k%= len(nums)
+        nums[:]= nums[-k:]+nums[:-k]
+        return nums
+        
+        
+        
+        
