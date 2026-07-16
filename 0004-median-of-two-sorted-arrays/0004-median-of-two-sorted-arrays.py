6b195d4f8b3a302b742331b68105c941d381a2b4
Giridhar Bargaley <153367525+Tsu-Giriiii@users.noreply.github.com>
2026-07-16 14:14:18 +0530

    Time: 0 ms (100%), Space: 12.4 MB (85.84%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -12,7 +12,7 @@ class Solution(object):
         else:
             return float(merged[(len(merged)/2)-1]+merged[(len(merged)/2)])/2'''
         
-        #Two pointer 
+        '''#Two pointer 
         count = 0
         i = 0
         j = 0
@@ -41,4 +41,36 @@ class Solution(object):
         if total_length%2!=0:
             return curr
         else:
-            return float(prev+curr)/2
+            return float(prev+curr)/2'''
+
+        #Binary Search
+        if len(nums1)>len(nums2):
+            return self.findMedianSortedArrays(nums2, nums1)
+
+        m = len(nums1)
+        n = len(nums2)
+
+        lo = 0
+        hi = m
+        half = (m+n+1)/2
+
+        while lo<=hi:
+            i = (lo+hi)/2
+            j = half - i
+
+            l1 = nums1[i-1] if not (i==0) else float('-inf')
+            r1 = nums1[i] if not (i==m) else float('inf')
+            l2 = nums2[j-1] if not (j==0) else float('-inf')
+            r2 = nums2[j] if not (j==n) else float('inf')
+            
+
+            if (l1<=r2 and l2<= r1) and (m+n)%2!=0:
+                return max(l1,l2)
+            
+            elif (l1<=r2 and l2<= r1) and (m+n)%2==0:
+                return (max(l1,l2)+min(r1,r2))/2.0
+            
+            elif l1>r2:
+                hi = i-1
+            else:
+                lo = i+1
