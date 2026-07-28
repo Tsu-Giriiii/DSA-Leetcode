a19fe5f68fe670253a293f1341045c2b21ee8a93
Giridhar Bargaley <153367525+Tsu-Giriiii@users.noreply.github.com>
2026-07-28 21:28:14 +0530

    Time: 541 ms (33.4%), Space: 12.5 MB (29.5%) - LeetHub

---
+++ 0018-4sum/0018-4sum.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        ans = []
+        for i in range(len(nums)-3):
+
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            for j in range(i+1,len(nums)-2):
+                
+                left = j+1
+                right = len(nums)-1
+                while left < right:
+                    sum1 = nums[i]+nums[j]+nums[right]+nums[left]
+
+                    if sum1 ==target and [nums[i],nums[j],nums[left],nums[right]] not in ans :
+                        ans.append([nums[i],nums[j],nums[left],nums[right]])
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+
+                        left += 1
+                        right -= 1
+
+                    elif sum1 < target:
+                        left += 1
+                    else:
+                        right -= 1
+
+
+        return ans
+
+        
+        
