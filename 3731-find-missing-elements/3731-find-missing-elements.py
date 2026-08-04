c15943dfc72a44f5cf8579ffe2ed6c172ca83154
Giridhar Bargaley <153367525+Tsu-Giriiii@users.noreply.github.com>
2026-08-04 14:52:02 +0530

    Time: 14 ms (7.83%), Space: 12.4 MB (17.28%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.py
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -4,7 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        nums.sort()
+        #Brute Force
+        '''nums.sort()
         first = nums[0]
         last = nums[-1]
         next_num = first
@@ -14,4 +15,15 @@ class Solution(object):
                 ans.append(next_num)
             next_num+= 1
 
+        return ans'''
+
+        #Better Approach
+        first = min(nums)
+        last = max(nums)
+
+        ans = []
+        for num in range(first,last):
+            if num not in nums:
+                ans.append(num)
+        
         return ans
