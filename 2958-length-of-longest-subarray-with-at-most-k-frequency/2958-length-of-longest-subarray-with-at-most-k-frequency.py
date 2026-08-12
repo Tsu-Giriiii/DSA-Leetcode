e691ee03829dc0314e7c216a50dcd1f437d39fed
Giridhar Bargaley <153367525+Tsu-Giriiii@users.noreply.github.com>
2026-08-12 17:30:33 +0530

    Time: 335 ms (68.61%), Space: 23.4 MB (5.11%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.py
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.py
@@ -5,7 +5,8 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        if not nums:
+        #Naive Approach
+        '''if not nums:
             return 0
         d = {}
         left = 0
@@ -18,4 +19,27 @@ class Solution(object):
             if len(d[nums[right]]) >k:
                 left = max(left,d[nums[right]][-k-1]+1)
             longest = max(longest,right-left+1)
+        return longest'''
+
+        #better approach
+        if not nums:
+            return 0
+        longest = 0
+        left = 0
+        count = {}
+
+        for right in range(len(nums)):
+            curr = nums[right]
+            if curr in count:
+                count[curr]+=1
+            else:
+                count[curr]=1
+            
+            while count[curr]>k:
+                left_slide = nums[left]
+                count[left_slide]-=1
+                left+=1
+            longest = max(longest,right-left+1)
         return longest
+
+
