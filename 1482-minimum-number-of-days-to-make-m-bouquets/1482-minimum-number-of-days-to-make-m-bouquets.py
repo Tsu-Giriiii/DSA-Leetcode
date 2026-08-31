b4d050feb54602d8bc16443aeaa62c7798104297
Giridhar Bargaley <153367525+Tsu-Giriiii@users.noreply.github.com>
2026-08-31 12:51:07 +0530

    Time: 443 ms (22.79%), Space: 32.5 MB (6.45%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
@@ -9,7 +9,7 @@ class Solution:
             
             mid = lo + (hi-lo)//2
 
-            if self.check(mid,bloomDay,m,k) and mid < res:
+            if self.check(mid,bloomDay,m,k):
                 res =mid
                 hi = mid -1
             else:
@@ -18,24 +18,17 @@ class Solution:
     
     def check(self,mid,bloomDay,m,k):
         # check if m distict groups of k values in bloomDay are less than mid
-
-        arr = [False]*(len(bloomDay))
-        for i in range(len(bloomDay)):
-            if bloomDay[i]<=mid:
-                arr[i] = True
         
-        left = 0
         count_groups = 0
         count_vals = 0
         for right in range(len(bloomDay)):
-            if arr[right]==True:
+            if bloomDay[right]<=mid:
                 count_vals+=1
                 if count_vals==k:
                     count_groups+=1
                     count_vals = 0
-                    left = right
+        
             else:
-                left = right
                 count_vals = 0
         
         return True if count_groups>=m else False
